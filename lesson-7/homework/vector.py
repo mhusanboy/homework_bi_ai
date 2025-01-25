198a6739c1fbd04919ac4260c65b04986c3074fd
mhusanboy <mrhusanboy2006pm@gmail.com>
2025-01-26 00:06:32 +0500

    modified vector.py

--- lesson-7/homework/vector.py
+++ lesson-7/homework/vector.py
@@ -1,4 +1,3 @@
-import math
 
 class Vector:
         
@@ -42,38 +41,11 @@ class Vector:
         return self.__mul__(other)
     
     def magnitude(self):
-        return math.sqrt(sum(x ** 2 for x in self.val))
+        return sum(x ** 2 for x in self.val)**0.5
     
     def normalize(self):
         m = self.magnitude()
+        if m == 0:
+            raise ValueError("Zero vector cannot be normalized!")
         return Vector(*[x / m for x in self.val])
-    
-# Create vectors
-v1 = Vector(1, 2, 3)
-v2 = Vector(4, 5, 6)
 
-# Print the vector
-print(v1)          # Output: Vector(1, 2, 3)
-
-# Addition
-v3 = v1 + v2
-print(v3)          # Output: Vector(5, 7, 9)
-
-# Subtraction
-v4 = v2 - v1
-print(v4)          # Output: Vector(3, 3, 3)
-
-# Dot product
-dot_product = v1 * v2
-print(dot_product) # Output: 32
-
-# Scalar multiplication
-v5 = 3 * v1
-print(v5)          # Output: Vector(3, 6, 9)
-
-# Magnitude
-print(v1.magnitude())  # Output: 3.7416573867739413
-
-# Normalization
-v_unit = v1.normalize()
-print(v_unit)      # Output: Vector(0.267, 0.534, 0.801)
